389de2d3819e37d5e804d8289971d9dda31d3c4d
kimlegeno <74814483+kimlegeno@users.noreply.github.com>
2022-01-16 16:04:45 +0900

    Make Random Background

---
+++ js/background.js
@@ -0,0 +1,9 @@
+const images = ["nature0.jpg", "nature1.jpg", "nature2.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
