16bcee3f38e267b1a80e75d7e77ea6a9524e8691
kimlegeno <74814483+kimlegeno@users.noreply.github.com>
2022-01-19 22:25:45 +0900

    make modal

--- js/greeting.js
+++ js/greeting.js
@@ -1,6 +1,7 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const bg = document.querySelector("div:first-child");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -8,6 +9,7 @@ const USERNAME_KEY = "username";
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
+  bg.className = "";
   const username = loginInput.value;
   localStorage.setItem("USERNAME_KEY", username);
   paintGreetings(username);
@@ -22,6 +24,7 @@ const savedUsername = localStorage.getItem("USERNAME_KEY");
 
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
+  bg.className = "bg--black";
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
   paintGreetings(savedUsername);
