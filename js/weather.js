6109e76becd77542ee9f1fb04ac3e62c78575cb5
kimlegeno <74814483+kimlegeno@users.noreply.github.com>
2022-01-21 13:10:47 +0900

    Modify

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,8 @@
 const API_KEY = "92e79a9e8ca3ef8a903753a40b909904";
+const weather = document.querySelector("#weather div span:last-child");
+const city = document.querySelector("#weather div span:first-child");
 
 function onGeoOk(position) {
-  const weather = document.querySelector("#weather span:last-child");
-  const city = document.querySelector("#weather span:first-child");
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
@@ -14,6 +14,7 @@ function onGeoOk(position) {
     });
 }
 function onGeoErr() {
+  city.innerText = "";
   weather.innerText = "I can't give you the weather information";
 }
 
